0393b7c9c9d20653822ab3c7955953a50ad24bf8
Tegan <teganmahanga@developersinstitute.co.nz>
2021-04-25 10:40:45 +1200

    added photos to static folder

--- src/homepage.js
+++ src/homepage.js
@@ -1,10 +1,8 @@
 const homepageSelector = document.querySelector("#homePageSelector");
 
-const welcome = () => {
-  const title = document.createElement("h1");
-  title.textContent = "I'm Tegz! Welcome to my World!";
-  const clickPrompt = document.createElement("p");
-  clickPrompt.textContent = "Click on the image to explore..";
-  homepageSelector.appendChild(title);
-  homepageSelector.appendChild(clickPrompt);
-};
+const title = document.createElement("h1");
+title.textContent = "Welcome to my World!";
+const clickPrompt = document.createElement("p");
+clickPrompt.textContent = "Click on the image to explore..";
+homepageSelector.appendChild(title);
+homepageSelector.appendChild(clickPrompt);
