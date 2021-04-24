9f3b7316576a05d873f396d2d62be5a3cd6cf027
Tegan <teganmahanga@developersinstitute.co.nz>
2021-04-25 09:59:32 +1200

    amended welcome message and added prompt to click image

--- src/homepage.js
+++ src/homepage.js
@@ -1,1 +1,8 @@
 const homepageSelector = document.querySelector("#homePageSelector");
+
+const welcome = () => {
+  const title = document.createElement("h1");
+  title.textContent = "I'm Tegz! Welcome to my World!";
+  const selectInfo = document.createElement("p");
+  selectInfo.textContent = "Click on the image to explore..";
+};
