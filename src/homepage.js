e7a00141616b18c8b2dc03643ba7e03d75526ef4
Tegan <teganmahanga@developersinstitute.co.nz>
2021-04-25 17:13:05 +1200

    solved issue with adding img to DOM - homepage image added

--- src/homepage.js
+++ src/homepage.js
@@ -1,13 +1,13 @@
 const homepageSelector = document.querySelector("#homePageSelector");
 
-// const photoDiv = createElement("div");
-const studentPhoto = createElement("img");
-// studentPhoto.src = "../static/studentPhoto.jpeg";
 const title = document.createElement("h1");
 title.textContent = "Welcome to my World";
 const clickPrompt = document.createElement("p");
 clickPrompt.textContent = "Click on the image to explore..";
-// photoDiv.appendChild(studentPhoto);
-// homepageSelector.appendChild(studentPhoto);
+const photoDiv = document.createElement("div");
+const studentPhoto = document.createElement("img");
+studentPhoto.src = "studentPhoto.jpeg";
+photoDiv.appendChild(studentPhoto);
+homepageSelector.appendChild(photoDiv);
 homepageSelector.appendChild(title);
 homepageSelector.appendChild(clickPrompt);
