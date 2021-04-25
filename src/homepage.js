508ed35f4476d898bd858ec8aa193d0b977b42b7
Tegan <teganmahanga@developersinstitute.co.nz>
2021-04-25 16:42:31 +1200

    working on dynamically adding photo to click on for homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,8 +1,13 @@
 const homepageSelector = document.querySelector("#homePageSelector");
 
+// const photoDiv = createElement("div");
+const studentPhoto = createElement("img");
+// studentPhoto.src = "../static/studentPhoto.jpeg";
 const title = document.createElement("h1");
-title.textContent = "Welcome to my World!";
+title.textContent = "Welcome to my World";
 const clickPrompt = document.createElement("p");
 clickPrompt.textContent = "Click on the image to explore..";
+// photoDiv.appendChild(studentPhoto);
+// homepageSelector.appendChild(studentPhoto);
 homepageSelector.appendChild(title);
 homepageSelector.appendChild(clickPrompt);
