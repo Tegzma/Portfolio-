748f1510fbd044484e40eb2d340eae07a374e797
Tegan <teganmahanga@developersinstitute.co.nz>
2021-04-30 20:17:53 +1200

    centered text on homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,7 @@
 const homepageSelector = document.querySelector("#homePageSelector");
 
 const title = document.createElement("h1");
+// new CircleType(title).radius(384);
 title.textContent = "Welcome to my World";
 const clickPrompt = document.createElement("p");
 clickPrompt.textContent = "Click on the image to explore..";
@@ -8,7 +9,7 @@ const photoDiv = document.createElement("div");
 const studentPhoto = document.createElement("img");
 studentPhoto.src = "../static/img/studentPhoto.jpeg";
 photoDiv.classList.add("homepage");
-photoDiv.appendChild(studentPhoto);
-homepageSelector.appendChild(photoDiv);
 homepageSelector.appendChild(title);
 homepageSelector.appendChild(clickPrompt);
+photoDiv.appendChild(studentPhoto);
+homepageSelector.appendChild(photoDiv);
